7f358c73ce1a4f360c03aaec292a0bec07264f0f
udaysharma0707 <sharma.uday8831@gmail.com>
2025-09-11 01:26:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // app.js - offline-first JSONP client (queueing + sequential flush + uppercase except services)
 // IMPORTANT: set ENDPOINT to your Apps Script web app URL and SHARED_TOKEN to the secret above
-const ENDPOINT = "https://script.google.com/macros/s/AKfycbx1Z-HYUN2QpG7aQ2AItEBXKlGanrtYh8xkH04rW5FTzHlbTKm9UcFgdLQfnqqcYoXQYQ/exec";
+const ENDPOINT = "https://script.google.com/macros/s/AKfycbxe_fSyPajKDnXMrhhXJM9MDykym3kLCiehDbFkKYimF_p1zKSOssGD0ibb78zAKNdurw/exec";
 const SHARED_TOKEN = "shopSecret2025";
 
 const KEY_QUEUE = "car_entry_queue_v1";
@@ -217,6 +217,7 @@ submitBtn.addEventListener('click', async function(){
     submitBtn.disabled = false; submitBtn.textContent = 'Submit';
   }
 });
+
 
 
 
