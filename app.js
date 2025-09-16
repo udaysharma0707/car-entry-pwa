57c775d8c75f3c45833a08403f60d40934684405
udaysharma0707 <sharma.uday8831@gmail.com>
2025-09-17 01:15:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // app.js - improved mobile-friendly client with JSONP queue & background send
 // IMPORTANT: set ENDPOINT to your Apps Script web app URL and SHARED_TOKEN to the secret above
-const ENDPOINT = "https://script.google.com/macros/s/AKfycbzEBWS40PGoLgf6Uw1ElM8ZNcT2xPe0sfRfdRsgi4ZaR3cebzsySA35DpCXT-6ao2J_uw/exec";
+const ENDPOINT = "https://script.google.com/macros/s/AKfycbyY9nQhDuoJjYqpqnjRTK7S7tkjL9vfTu3VxzQWelifoRQzHR4sOjiH7BSbn0bTS9W23Q/exec";
 const SHARED_TOKEN = "shopSecret2025";
 const KEY_QUEUE = "car_entry_queue_v1";
 
@@ -323,6 +323,7 @@ document.addEventListener('DOMContentLoaded', function() {
   }, 300);
 
 }); // DOMContentLoaded end
+
 
 
 
